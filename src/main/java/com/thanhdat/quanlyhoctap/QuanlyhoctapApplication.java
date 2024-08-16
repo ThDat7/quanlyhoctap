e5d3f6008c1a9ac21c4ddad2ca94cf6d02432a8b
ntd13579 <mailreport10@gmail.com>
2024-08-16 16:12:31 +0700

    feat(codebase): add codebase project.

---
+++ src/main/java/com/thanhdat/quanlyhoctap/QuanlyhoctapApplication.java
@@ -0,0 +1,13 @@
+package com.thanhdat.quanlyhoctap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyhoctapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyhoctapApplication.class, args);
+	}
+
+}
