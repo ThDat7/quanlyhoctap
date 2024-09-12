cbc3091fe9b69c126f94a5c8c0fe302484eb8a4b
ntd13579 <mailreport10@gmail.com>
2024-09-12 17:10:49 +0700

    fix(specifications): Change logic join attribute to avoid join multiple times by jpa-specification.

--- src/main/java/com/thanhdat/quanlyhoctap/specification/CourseOutlineSpecification.java
+++ src/main/java/com/thanhdat/quanlyhoctap/specification/CourseOutlineSpecification.java
@@ -1,11 +1,17 @@
 package com.thanhdat.quanlyhoctap.specification;
 
 import com.thanhdat.quanlyhoctap.entity.*;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
+import static com.thanhdat.quanlyhoctap.specification.JoinHelper.join;
 
 public class CourseOutlineSpecification {
     public static Specification<CourseOutline> courseNameLike(String kw) {
-        return (root, query, cb) -> cb.like(root.get(CourseOutline_.course).get(Course_.name), "%" + kw + "%");
+        return (root, query, cb) -> {
+            Join<CourseOutline, Course> courseJoin = join(root, CourseOutline_.course, JoinType.INNER);
+            return cb.like(courseJoin.get(Course_.name), "%" + kw + "%");
+        };
     }
 
     public static Specification<CourseOutline> statusEqual(OutlineStatus status) {
@@ -13,14 +19,30 @@ public class CourseOutlineSpecification {
     }
 
     public static Specification<CourseOutline> courseEqual(Integer courseId) {
-        return (root, query, cb) -> cb.equal(root.get(CourseOutline_.course).get(Course_.id), courseId);
+        return (root, query, cb) -> {
+            Join<CourseOutline, Course> courseJoin = join(root, CourseOutline_.course, JoinType.INNER);
+            return cb.equal(courseJoin.get(Course_.id), courseId);
+        };
     }
 
     public static Specification<CourseOutline> belongsToMajor(Integer majorId) {
-        return (root, query, cb) -> cb.equal(root
-                .join(CourseOutline_.educationProgramCourses)
-                .join(EducationProgramCourse_.educationProgram)
-                .join(EducationProgram_.major).get(Major_.id), majorId);
+        return (root, query, cb) -> {
+            Join<CourseOutline, EducationProgramCourse> educationProgramCourseJoin = join(root, CourseOutline_.educationProgramCourses, JoinType.INNER);
+            Join<EducationProgramCourse, EducationProgram> educationProgramJoin = join(educationProgramCourseJoin, EducationProgramCourse_.educationProgram, JoinType.INNER);
+            Join<EducationProgram, Major> majorJoin = join(educationProgramJoin, EducationProgram_.major, JoinType.INNER);
+            return cb.equal(majorJoin.get(Major_.id), majorId);
+        };
+    }
+
+    public static Specification<CourseOutline> belongsToTeacherId(Integer teacherId) {
+        return (root, query, cb) -> {
+            Join<CourseOutline, Teacher> teacherJoin = join(root, CourseOutline_.teacher, JoinType.INNER);
+            return cb.equal(teacherJoin.get(Teacher_.id), teacherId);
+        };
+    }
+
+    public static Specification<CourseOutline> idEqual(Integer id) {
+        return (root, query, cb) -> cb.equal(root.get(CourseOutline_.id), id);
     }
 
     public static Specification<CourseOutline> yearPublishedEqual(Integer year) {
@@ -28,15 +50,19 @@ public class CourseOutlineSpecification {
     }
 
     public static Specification<CourseOutline> belongsToEducationProgramBySchoolYear(Integer year) {
-        return (root, query, cb) -> cb.equal(root
-                .join(CourseOutline_.educationProgramCourses)
-                .join(EducationProgramCourse_.educationProgram).get(EducationProgram_.schoolYear), year);
+        return (root, query, cb) -> {
+            Join<CourseOutline, EducationProgramCourse> educationProgramCourseJoin = join(root, CourseOutline_.educationProgramCourses, JoinType.INNER);
+            Join<EducationProgramCourse, EducationProgram> educationProgramJoin = join(educationProgramCourseJoin, EducationProgramCourse_.educationProgram, JoinType.INNER);
+            return cb.equal(educationProgramJoin.get(EducationProgram_.schoolYear), year);
+        };
     }
 
     public static Specification<CourseOutline> teacherNameLike(String kw) {
-        return (root, query, cb) -> cb.like(cb.concat(
-                cb.concat(root.get(CourseOutline_.teacher).get(Teacher_.user).get(User_.lastName), " "),
-                        root.get(CourseOutline_.teacher).get(Teacher_.user).get(User_.firstName)), "%" + kw + "%");
+        return (root, query, cb) -> {
+            Join<CourseOutline, Teacher> teacherJoin = join(root, CourseOutline_.teacher, JoinType.INNER);
+            Join<Teacher, User> userJoin = join(teacherJoin, Teacher_.user, JoinType.INNER);
+            return cb.like(cb.concat(cb.concat(userJoin.get(User_.lastName), " "), userJoin.get(User_.firstName)), "%" + kw + "%");
+        };
     }
 
     public static Specification<CourseOutline> haveUrl() {
@@ -49,6 +75,9 @@ public class CourseOutlineSpecification {
     }
 
     public static Specification<CourseOutline> courseCreditsEqual(float credits) {
-        return (root, query, cb) -> cb.equal(root.get(CourseOutline_.course).get(Course_.credits), credits);
+        return (root, query, cb) -> {
+            Join<CourseOutline, Course> courseJoin = join(root, CourseOutline_.course, JoinType.INNER);
+            return cb.equal(courseJoin.get(Course_.credits), credits);
+        };
     }
 }
--- src/main/java/com/thanhdat/quanlyhoctap/specification/EducationProgramSpecification.java
+++ src/main/java/com/thanhdat/quanlyhoctap/specification/EducationProgramSpecification.java
@@ -2,13 +2,21 @@ package com.thanhdat.quanlyhoctap.specification;
 
 import com.thanhdat.quanlyhoctap.entity.EducationProgram;
 import com.thanhdat.quanlyhoctap.entity.EducationProgram_;
+import com.thanhdat.quanlyhoctap.entity.Major;
 import com.thanhdat.quanlyhoctap.entity.Major_;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
+import static com.thanhdat.quanlyhoctap.specification.JoinHelper.join;
 
 public class EducationProgramSpecification {
 
     public static Specification<EducationProgram> nameLike(String kw) {
-        return (root, query, cb) -> cb.like(root.get(EducationProgram_.major).get(Major_.name), "%" + kw + "%");
+
+        return (root, query, cb) -> {
+            Join<EducationProgram, Major> majorJoin = join(root, EducationProgram_.major, JoinType.INNER);
+            return cb.like(majorJoin.get(Major_.name), "%" + kw + "%");
+        };
     }
 
     public static Specification<EducationProgram> schoolYearEqual(int year) {
@@ -16,6 +24,9 @@ public class EducationProgramSpecification {
     }
 
     public static Specification<EducationProgram> majorEqual(int majorId) {
-        return (root, query, cb) -> cb.equal(root.get(EducationProgram_.major).get(Major_.id), majorId);
+        return (root, query, cb) -> {
+            Join<EducationProgram, Major> majorJoin = join(root, EducationProgram_.major, JoinType.INNER);
+            return cb.equal(majorJoin.get(Major_.id), majorId);
+        };
     }
 }
